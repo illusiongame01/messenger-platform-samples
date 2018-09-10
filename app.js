a2b80340c7aa4a61ec835c33d3926f5da6622217
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-09-10 15:27:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ app.post('/webhook', (req, res) => {
     // Parse the request body from the POST
     let body = req.body;
        console.log('Webhook');
+    console.log(body);
     // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
 
